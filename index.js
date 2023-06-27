9b2c687ddec2d7be3dddecc789e5acc2b937675f
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-27 13:06:22 +0100

    cookie-parser

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import db from './connectDB.js'
-
+import cookieParser from 'cookie-parser'
 import customerRoute from './routes/customer.js'
+import cookieRoutes from './routes/cookie.js'
 import cors from 'cors'
 
 const app = express()
@@ -11,8 +12,9 @@ db()
 
 app.use(cors())
 app.use(express.json())
-
+app.use(cookieParser('mysecret'))
 app.use('/customer', customerRoute)
+app.use('/cookie', cookieRoutes)
 
 app.get('/', (req, res) => {
   res.send('hello')

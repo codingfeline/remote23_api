78aba33c1835c87678068f2debd3b3f700552293
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-28 21:43:11 +0100

    add catchAsync

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 import { express, User } from '../imports.js'
+import catchAsync from '../utils/catchAsync.js'
 
 const router = express.Router()
 
@@ -11,5 +12,19 @@ router.get('/fakeUser', async (req, res) => {
   const newUser = await User.register(user, 'password')
   res.send(newUser)
 })
+
+router.post(
+  '/register',
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body
+      const user = new User({ email, username })
+      const registeredUser = await User.register(user, password)
+      res.send(registeredUser)
+    } catch (e) {
+      res.send(e.message)
+    }
+  })
+)
 
 export default router
